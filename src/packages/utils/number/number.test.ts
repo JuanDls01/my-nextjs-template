c101542ce9cbc108055e0f59ad990e0f5ba8df03
JuanDls01 <juanignaciodelossantos01@gmail.com>
2024-05-28 21:26:09 -0300

    feat(numbers-utils): create number utils

---
+++ src/packages/utils/number/number.test.ts
@@ -0,0 +1,1 @@
+// Here I should add unit tests for number.ts file.
---
+++ src/packages/utils/number/number.ts
@@ -0,0 +1,4 @@
+export const isNumber = (input: unknown): input is NonNullable<number> =>
+  typeof input === 'number' && Number.isFinite(input)
+
+export const beginsWithNumber = (input: string) => /^[0-9]/.test(input)
